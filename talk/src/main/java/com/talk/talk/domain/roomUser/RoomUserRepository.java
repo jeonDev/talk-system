81e62aa65e7db55c1c1d8ba9a52bae6d836d574c
jeonDev <jjhwhdgus0727@gmail.com>
2023-09-24 20:16:13 +0900

    RoomUserRepository PK Update

--- talk/src/main/java/com/talk/talk/domain/roomUser/RoomUserRepository.java
+++ talk/src/main/java/com/talk/talk/domain/roomUser/RoomUserRepository.java
@@ -1,7 +1,9 @@
 package com.talk.talk.domain.roomUser;
 
-import com.talk.talk.domain.room.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomUserRepository extends JpaRepository<RoomUser, Room> {
+import java.util.List;
+
+public interface RoomUserRepository extends JpaRepository<RoomUser, RoomUserId> {
+    List<RoomUser> findByRoomSeq(Long roomSeq);
 }
