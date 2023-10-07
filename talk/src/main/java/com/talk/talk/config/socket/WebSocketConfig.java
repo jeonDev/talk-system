d2dc938165228ef1d6f1e1b2d85d499b603936a4
jeonDev <jjhwhdgus0727@gmail.com>
2023-10-07 15:51:50 +0900

    ChatHandler DI

--- talk/src/main/java/com/talk/talk/config/socket/WebSocketConfig.java
+++ talk/src/main/java/com/talk/talk/config/socket/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.talk.talk.config.socket;
 
 import com.talk.talk.config.interceptor.WebSocketInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -14,9 +15,12 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Value("${cors.url}")
     private String corsUrl;
 
+    @Autowired
+    private ChatHandler chatHandler;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatHandler(), "/ws")
+        registry.addHandler(chatHandler, "/ws")
                 .setAllowedOrigins(corsUrl);
     }
 }
